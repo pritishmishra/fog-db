7baee39f953298393f1c0d341d0d5a01f614cecc
craig14 <adarshpratik.2010@gmail.com>
2018-05-20 03:37:50 +0530

    adding client file for client-server connection

---
+++ server/JabberClientThread.java
@@ -0,0 +1,66 @@
+import java.net.*;
+import java.io.*;
+ 
+class JabberClientThread extends Thread {
+  private Socket socket;
+  private BufferedReader in;
+  private PrintWriter out;
+  private static int counter = 0;
+  private int id = counter++;
+  private static int threadcount = 0;
+  public static int threadCount() { 
+    return threadcount; 
+  }
+  public JabberClientThread() {
+    System.out.println("Making client " + id);
+    threadcount++;
+    try {
+      socket = new Socket("192.168.43.32", 9999);
+    } catch(IOException e) {
+      // If the creation of the socket fails, 
+      // nothing needs to be cleaned up.
+    }
+    try {    
+      in = 
+        new BufferedReader(
+          new InputStreamReader(
+            socket.getInputStream()));
+      // Enable auto-flush:
+      out = 
+        new PrintWriter(
+          new BufferedWriter(
+            new OutputStreamWriter(
+              socket.getOutputStream())), true);
+      start();
+    } catch(IOException e) {
+      // The socket should be closed on any 
+      // failures other than the socket 
+      // constructor:
+      try {
+        socket.close();
+      } catch(IOException e2) {}
+    }
+    // Otherwise the socket will be closed by
+    // the run() method of the thread.
+  }
+  public void run() {
+    try {
+      /*for(int i = 0; i < 25; i++) {
+        out.println("Client " + id + ": " + i);
+        String str = in.readLine();
+        System.out.println(str);
+      }
+      out.println("END");*/
+    	DataOutputStream dos=new DataOutputStream(socket.getOutputStream());
+    	dos.writeUTF("This is client 1");
+    	out.println("END");
+    } catch(IOException e) {
+    } finally {
+      // Always close it:
+      try {
+        socket.close();
+      } catch(IOException e) {}
+      threadcount--; // Ending this thread
+    }
+  }
+}
