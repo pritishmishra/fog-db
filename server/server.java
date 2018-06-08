6c4a4385da91e73c1b1cde3e70901596feb94f6b
craig14 <adarshpratik.2010@gmail.com>
2018-06-08 20:46:38 +0530

    renaming server.java to Server.java

--- server/server.java
+++ server/server.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class server {
+public class Server {
 
 public static void main(String args[]) throws IOException{  
 
